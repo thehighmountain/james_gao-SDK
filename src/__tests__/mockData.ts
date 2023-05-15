03d0b4de0734d3880d99e86b6e6c8aa8910da9c0
thehighmountain <thehighmountain25@gmail.com>
2023-05-15 14:44:13 -0400

    test data added

---
+++ src/__tests__/mockData.ts
@@ -0,0 +1,95 @@
+export const test_movies = [
+  {
+    id: "m001",
+    title: "The Fellowship of the Ring",
+    quotes: [
+      {
+        id: "q001",
+        quote: "The Fellowship of the Ring 001"
+      },
+      {
+        id: "q002",
+        quote: "The Fellowship of the Ring 002"
+      },
+      {
+        id: "q003",
+        quote: "The Fellowship of the Ring 003"
+      }
+    ]
+  },
+  {
+    id: "m002",
+    title: "The Two Towers",
+    quotes: [
+      {
+        id: "m2q001",
+        quote: "The Two Towers 001"
+      },
+      {
+        id: "m2q002",
+        quote: "The Two Towers 002"
+      },
+      {
+        id: "m2q003",
+        quote: "The Two Towers 003"
+      }
+    ]
+  },
+  {
+    id: "m003",
+    title: "The Return of the King",
+    quotes: [
+      {
+        id: "m3q001",
+        quote: "The Return of the King 001"
+      },
+      {
+        id: "m3q002",
+        quote: "The Return of the King 002"
+      },
+      {
+        id: "m3q003",
+        quote: "The Return of the King 003"
+      }
+    ]
+  }
+]
+
+export const test_quotes = [
+  {
+    id: "q001",
+    quote: "The Fellowship of the Ring 001"
+  },
+  {
+    id: "q002",
+    quote: "The Fellowship of the Ring 002"
+  },
+  {
+    id: "q003",
+    quote: "The Fellowship of the Ring 003"
+  },
+  {
+    id: "m2q001",
+    quote: "The Two Towers 001"
+  },
+  {
+    id: "m2q002",
+    quote: "The Two Towers 002"
+  },
+  {
+    id: "m2q003",
+    quote: "The Two Towers 003"
+  },
+  {
+    id: "m3q001",
+    quote: "The Return of the King 001"
+  },
+  {
+    id: "m3q002",
+    quote: "The Return of the King 002"
+  },
+  {
+    id: "m3q003",
+    quote: "The Return of the King 003"
+  }
+]
